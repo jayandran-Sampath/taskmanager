2933444fd776a9144435795e51f48d2638ad2950
jayandran sampath <jaidg2012@gmail.com>
2023-06-26 18:12:25 +0100

    Handle App Layer with test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("Task Manager",() => {
+  
+    test('renders App Header', () => {
+      render(<App />);
+      const headerTitle = screen.getByText(/Task Manager/i);
+      expect(headerTitle).toBeInTheDocument();
+    });
+  
+    test('renders default tasks', () => {
+      render(<App />);
+      const defaultTaskDescription = screen.getByText(/Doctor appointment/i);
+      expect(defaultTaskDescription).toBeInTheDocument();
+    });
+  
+    test('remove Task when delete button is clicked', () => {
+      render(<App />);
+      fireEvent.click(screen.getByTestId('task-1-delete'))
+      const defaultTaskDescription = screen.queryByText('Doctor appointment');
+      expect(defaultTaskDescription).not.toBeInTheDocument();
+    });
+  
+    test('render add Task form when add button is clicked', () => {
+      render(<App />);
+      fireEvent.click(screen.getByTestId('AddBtn'))
+      const addTaskForm = screen.getByTestId('add-task-form');
+      expect(addTaskForm).toBeInTheDocument();
+    });
+  
+    test('closes add Task form when close button is clicked', () => {
+      render(<App />);
+      fireEvent.click(screen.getByTestId('AddBtn'))
+      fireEvent.click(screen.getByTestId('CloseBtn'));
+      const addFormText = screen.queryByText('Add Your task here');
+      expect(addFormText).not.toBeInTheDocument();
+    });
+  
+  })
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './App.css'
+import { Header } from './components/Header';
+import { Task } from './components/Task';
+import { TaskModel } from './model/TaskModel';
 
 function App() {
+  const taskModel: TaskModel[] = JSON.parse(`
+  [
+    {
+      "id" : 1,
+      "description" : "Doctor appointment",
+      "day" : "23rd June 2023",
+      "reminder" : true
+    },
+    {
+      "id" : 2,
+      "description" : "First Date",
+      "day" : "24th June 2023",
+      "reminder" : false
+    },
+    {
+      "id" : 3,
+      "description" : "Shopping at trafford",
+      "day" : "25th June 2023",
+      "reminder" : false
+    }    
+  ]
+  `)
+  const [tasks, setTasks] = useState(taskModel)
+  const removeTask = (id: number) => {
+    setTasks(tasks.filter(task => task.id != id))
+  }
+
+  const addTask = (data: TaskModel) => {
+    setTasks([...tasks, data])
+  }
+
+  const setReminder = (id: number) => {
+    setTasks(tasks.map((t) => {
+      if (t.id === id) {
+        t.reminder = !t.reminder
+      }
+      return t
+    }))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="Task Manager" addTask={addTask} />
+      {tasks.map(task => <Task key={task.id} value={task} handleClick={removeTask} setReminder={setReminder} />)}
     </div>
   );
 }
-
 export default App;
